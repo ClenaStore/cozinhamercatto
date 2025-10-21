0f0fcdb61ac5440b19da3e755f20f9cc085440dc
clena <storeclena@gmail.com>
2025-10-21 09:42:55 -0300

    Update and rename env to env.js

--- api/env.js
+++ api/env.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
-  // Lê a senha secreta da variável de ambiente da Vercel
+  // Lê a senha armazenada como variável de ambiente na Vercel
   const senha = process.env.SENHA_COZINHA || "";
 
-  // Retorna apenas via JSON (sem expor outras envs)
+  // Retorna apenas o valor necessário, sem expor outras envs
   res.status(200).json({ SENHA_COZINHA: senha });
 }
